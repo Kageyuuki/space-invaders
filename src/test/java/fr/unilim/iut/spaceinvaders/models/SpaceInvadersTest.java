b73fc56241d7335e50e4926a96a7a5d94b6c1bc4
Sophie Jacobs <sojacobs@hotmail.fr>
2018-05-07 14:52:06 +0200

    Commit changement package

---
+++ src/test/java/fr/unilim/iut/spaceinvaders/models/SpaceInvadersTest.java
@@ -0,0 +1,55 @@
+package fr.unilim.iut.spaceinvaders.models;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import fr.unilim.iut.spaceinvaders.models.SpaceInvaders;
+import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
+
+
+
+public class SpaceInvadersTest {
+
+   @Test
+   public void test_AuDebut_JeuSpaceInvaderEstVide() {
+	    SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+	    assertEquals("" + 
+	    "...............\n" + 
+	    "...............\n" +
+	    "...............\n" + 
+	    "...............\n" + 
+	    "...............\n" + 
+	    "...............\n" + 
+	    "...............\n" + 
+	    "...............\n" + 
+	    "...............\n" + 
+	    "...............\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
+        }
+   
+   
+   @Test
+	public void test_unNouveauVaisseauEstCorrectementPositionneDansEspaceJeu() {
+		SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+		spaceinvaders.positionnerUnNouveauVaisseau(7,9);
+		assertEquals("" + 
+		"...............\n" + 
+		"...............\n" +
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		".......V.......\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
+	}
+   
+   @Test(expected = HorsEspaceJeuException.class)
+	public void test_unNouveauVaisseauEstPositionneHorsEspaceJeuTropADroite_UneExceptionEstLevee() throws Exception {
+		SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+		spaceinvaders.positionnerUnNouveauVaisseau(15,9);
+	}
+
+   }
+
